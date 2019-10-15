445d50e77bfb2e21fa37817a1698fc52a320906c
f j w <fjw@fdeMacBook-Pro.local>
2019-10-15 15:49:08 +0800

    next

---
+++ src/main/java/com/yqvod/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.yqvod.service;
+
+/**
+ * 项目名称：yqvod
+ * 类 名 称：IUserService
+ * 类 描 述：TODO
+ * 创建时间：2019/10/15 3:39 PM
+ * 创 建 人：fjw
+ */
+public interface IUserService {
+    Object login(String username,String password);
+}
---
+++ src/main/java/com/yqvod/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.yqvod.service.impl;
+/**
+ * 项目名称：yqvod
+ * 类 名 称：UserServiceImpl
+ * 类 描 述：TODO
+ * 创建时间：2019/10/15 3:41 PM
+ * 创 建 人：fjw
+ */
+
+import com.yqvod.service.IUserService;
+
+/**
+ * @ClassName $ {NAME}
+ * @Description TODO
+ * @Author fjw
+ * @Date 2019/10/15 3:41 PM
+ * @Version 1.0
+ **/
+public class UserServiceImpl implements IUserService {
+    @Override
+    public Object login(String username, String password) {
+        return null;
+    }
+}
