6ba52a7cf9fd18cca7a9595672c706da67776420
dtvikey888 <dtvikey@163.com>
2019-10-31 06:31:17 +0800

    ok

--- src/main/java/com/yqvod/util/TranceData.java
+++ src/main/java/com/yqvod/util/TranceData.java
@@ -1,6 +1,7 @@
 package com.yqvod.util;
 
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 /**
  * @ClassName $ {NAME}
@@ -11,17 +12,25 @@ import java.sql.ResultSet;
  **/
 public class TranceData {
 
-    public static void main(String[] args) {
 
-        for (int i = 0; i < 10 ; i++) {
-            System.out.println("***");
-            for (int j = 0; j < 60 ; j++) {
-                System.out.println("******");
+        public static void main(String[] args){
+            int i,j,k,n;
+            Scanner input=new Scanner(System.in);
+            System.out.print("请输入金字塔层数：");
+            n=input.nextInt();//外层循环控制层数
+            for(i=1;i<=n;i++)
+            {
+                //根据外层行号，输出星号左边空格
+                for(j=1;j<=n-i;j++)
+                    System.out.print(" ");//根据外层行号，输出星号个数
+                for(k=1;k<=2*i-1;k++)
+                    System.out.printf("*");//一行结束，换行
+                System.out.printf("\n");
             }
 
         }
 
-    }
+
 
     /**
     public static void main(String[] args) {
