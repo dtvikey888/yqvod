3e13f6f4478578268e03ac69b996ee06706bfa5e
dtvikey888 <dtvikey@163.com>
2019-11-26 20:26:20 +0800

    tt

--- src/main/java/com/yqvod/controller/backend/UserManageController.java
+++ src/main/java/com/yqvod/controller/backend/UserManageController.java
@@ -23,7 +23,7 @@ public class UserManageController {
     @Autowired
     private IUserService iUserService;
 
-    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @RequestMapping(value = "login2.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
         ServerResponse<User> response = iUserService.login(username,password);
